124d4e6c0ac8c72fcdd68a1483335dcbc568e7e7
Ian Munro <i.munro@imperial.ac.uk>
2012-08-17 18:31:55 +0100

    Added create_new_dataset to version control.
    --HG--
    branch : OMERO_TCSPC

---
+++ GlobalProcessingFrontEnd/OMEROUtilities/create_new_Dataset.m
@@ -0,0 +1,37 @@
+        function new_dataset = create_new_Dataset(session,project,new_dataset_name,description)
+            new_dataset = [];
+            %
+            newdataset = omero.model.DatasetI;              
+            newdataset.setName(omero.rtypes.rstring(char(new_dataset_name)));
+            newdataset.setDescription(omero.rtypes.rstring(char(description)));
+            %link Dataset and Project
+            link = omero.model.ProjectDatasetLinkI;
+            link.setChild(newdataset);            
+            link.setParent(omero.model.ProjectI(project.getId().getValue(),false));            
+            session.getUpdateService().saveAndReturnObject(link);                                                
+            % find newly created dataset by name
+            
+            proxy = session.getContainerService();
+            param = omero.sys.ParametersI();
+            param.noLeaves(); %no images loaded, this is the default value.
+            userId = session.getAdminService().getEventContext().userId;
+            param.exp(omero.rtypes.rlong(userId));            
+            projectsList = proxy.loadContainerHierarchy('omero.model.Project', [], param);            
+            % there is no check of existing dataset name - the same name
+            % might appear many times..
+            for j = 0:projectsList.size()-1,
+                p = projectsList.get(j);                
+                datasetsList = p.linkedDatasetList;
+                for i = 0:datasetsList.size()-1,
+                    d = datasetsList.get(i);
+                    dName = char(java.lang.String(d.getName().getValue()));                                        
+                    % 
+                    if strcmp(new_dataset_name,dName) && java.lang.Long(p.getId().getValue()) == java.lang.Long(project.getId().getValue())
+                        new_dataset = d;
+                        break;
+                    end;                    
+                end;
+            end;                                                                                
+            %
+        end  
+        
