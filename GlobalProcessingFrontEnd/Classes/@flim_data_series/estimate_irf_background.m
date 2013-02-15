82e36f9cacc67db96b3256f816928ff42d2f77b9
Ian Munro <i.munro@imperial.ac.uk>
2013-02-15 16:55:06 +0000

    2nd set of headers: flim_data_series

--- GlobalProcessingFrontEnd/Classes/@flim_data_series/estimate_irf_background.m
+++ GlobalProcessingFrontEnd/Classes/@flim_data_series/estimate_irf_background.m
@@ -1,11 +1,38 @@
 function estimate_irf_background(obj)
     
-    %> Estimate the IRF background level.
+%> Estimate the IRF background level.
     
-    % Try to fit to two gaussians, one for peak and one for background. 
-    % Theoretically not as good as fitting to a gauss + constant but seems 
-    % to converge better. In general the background just has a very large 
-    % sigma. The background is the mean of the smaller 'peak'
+% Try to fit to two gaussians, one for peak and one for background. 
+% Theoretically not as good as fitting to a gauss + constant but seems 
+% to converge better. In general the background just has a very large 
+% sigma. The background is the mean of the smaller 'peak'
+    
+    
+% Copyright (C) 2013 Imperial College London.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
+% This software tool was developed with support from the UK 
+% Engineering and Physical Sciences Council 
+% through  a studentship from the Institute of Chemical Biology 
+% and The Wellcome Trust through a grant entitled 
+% "The Open Microscopy Environment: Image Informatics for Biological Sciences" (Ref: 095931).
+
+% Author : Sean Warren
+
 
     for i=1:size(obj.irf,2)
 
