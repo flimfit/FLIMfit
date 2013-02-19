46eff1896fb9d44c99a72bf8f630cfab0cb38a48
Ian Munro <i.munro@imperial.ac.uk>
2013-02-19 19:14:17 +0000

    More headers! Remaining classes.

--- GlobalProcessingFrontEnd/Classes/@flim_omero_data_manager/load_irf_from_Dataset.m
+++ GlobalProcessingFrontEnd/Classes/@flim_omero_data_manager/load_irf_from_Dataset.m
@@ -1,5 +1,31 @@
 function load_irf_from_Dataset(obj,data_series,dataset,load_as_image)
 
+
+    % Copyright (C) 2013 Imperial College London.
+    % All rights reserved.
+    %
+    % This program is free software; you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation; either version 2 of the License, or
+    % (at your option) any later version.
+    %
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License along
+    % with this program; if not, write to the Free Software Foundation, Inc.,
+    % 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    %
+    % This software tool was developed with support from the UK 
+    % Engineering and Physical Sciences Council 
+    % through  a studentship from the Institute of Chemical Biology 
+    % and The Wellcome Trust through a grant entitled 
+    % "The Open Microscopy Environment: Image Informatics for Biological Sciences" (Ref: 095931).
+
+  
+
     [t_irf im_data] = obj.load_FLIM_data_from_Dataset(dataset);                                
     %
     irf_image_data = double(im_data);
