e1688be15e0202430b55ac8643e799db4e1057b3
Ian Munro <i.munro@imperial.ac.uk>
2013-02-21 12:21:02 +0000

    Headers: Yuriy Segmentation.

--- GlobalProcessingFrontEnd/YuriySegmentation/nth_dark_difference.m
+++ GlobalProcessingFrontEnd/YuriySegmentation/nth_dark_difference.m
@@ -10,6 +10,34 @@ function z = nth_dark_difference(U,scale1,rel_bg_scale1,threshold1,scale2,rel_bg
 %smoothing,Radius of smoothing kernel (pixels)
 %min_area,Minimium object area (pixel^2)
 
+
+
+
+%
+% Copyright (C) 2013 Imperial College London.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
+% This software tool was developed with support from the UK 
+% Engineering and Physical Sciences Council 
+% through  a studentship from the Institute of Chemical Biology 
+% and The Wellcome Trust through a grant entitled 
+% "The Open Microscopy Environment: Image Informatics for Biological Sciences" (Ref: 095931).
+
+
 S1 = scale1;
 S2 = scale2;
 K1 = rel_bg_scale1;
