7a34c04f41a8d683d219c03cb0f6762c99c4ea77
yalexand@ph-yalexandlap.ic.ac.uk <yalexand@ph-yalexandlap.ic.ac.uk>
2013-03-21 12:02:30 +0000

    faster "whos_Object" function - eventually..

--- GlobalProcessingFrontEnd/OMEROUtilities/whos_Object.m
+++ GlobalProcessingFrontEnd/OMEROUtilities/whos_Object.m
@@ -23,47 +23,32 @@ function obj_type = whos_Object(session,objId)
 % and The Wellcome Trust through a grant entitled 
 % "The Open Microscopy Environment: Image Informatics for Biological Sciences" (Ref: 095931).
                     
-            
             obj_type = 'unknown';            
             %
-            proxy = session.getContainerService();
-            %Set the options
-            param = omero.sys.ParametersI();
-            %
-            param.leaves();
-            %
+            param = omero.sys.ParametersI();            
+            param.leaves();            
             userId = session.getAdminService().getEventContext().userId; %id of the user.
             param.exp(omero.rtypes.rlong(userId));
-            projectsList = proxy.loadContainerHierarchy('omero.model.Project', [], param);
-            %
-            for j = 0:projectsList.size()-1,
-                p = projectsList.get(j);
-                pid = java.lang.Long(p.getId().getValue());                
-                if pid == objId
-                    obj_type = 'Project';
-                    return;
-                end;
-            end;
-            datasetsList = proxy.loadContainerHierarchy('omero.model.Dataset', [], param);
-                for i = 0:datasetsList.size()-1,                     
-                     d = datasetsList.get(i);
-                     did = java.lang.Long(d.getId().getValue());
-                     if did == objId
-                        obj_type = 'Dataset';
-                        return;
-                     end
-                     imageList = d.linkedImageList;
-                     for k = 0:imageList.size()-1,                       
-                         img = imageList.get(k);
-                         iid = java.lang.Long(img.getId().getValue());
-                         if iid == objId
-                            obj_type = 'Image';
-                            return;
-                         end;
-                     end;
-                end;
-            %
-                    iQuery = session.getQueryService();
+                                        
+            myprojects = getProjects(session,objId);
+            if ~isempty(myprojects) 
+                obj_type = 'Project';
+                return;
+            end
+            
+            mydatasets = getDatasets(session,objId);
+            if ~isempty(mydatasets) 
+                obj_type = 'Dataset';
+                return;
+            end
+            
+            myimages = getImages(session,objId);
+            if ~isempty(myimages) 
+                obj_type = 'Image';
+                return;
+            end
+                        
+            iQuery = session.getQueryService();
                     screenList = iQuery.findAllByQuery('select this from Screen this left outer join fetch this.plateLinks links left outer join fetch links.child plates', param);                                
                     for k = 0:screenList.size()-1,                       
                          scr = screenList.get(k);
@@ -73,37 +58,13 @@ function obj_type = whos_Object(session,objId)
                             return;
                          end  
                          platesList = scr.linkedPlateList;
-                         for k = 0:platesList.size()-1,                       
-                             plt = platesList.get(k);
+                         for m = 0:platesList.size()-1,                       
+                             plt = platesList.get(m);
                              pltid = java.lang.Long(plt.getId().getValue());
                              if pltid == objId
                                 obj_type = 'Plate';
                                 return;
                              end  
-                             %
-                            wellList = session.getQueryService().findAllByQuery(['select well from Well as well '...
-                            'left outer join fetch well.plate as pt '...
-                            'left outer join fetch well.wellSamples as ws '...
-                            'left outer join fetch ws.plateAcquisition as pa '...
-                            'left outer join fetch ws.image as img '...
-                            'left outer join fetch img.pixels as pix '...
-                            'left outer join fetch pix.pixelsType as pt '...
-                            'where well.plate.id = ', num2str(plt.getId().getValue())],[]);
-                            for j = 0:wellList.size()-1,
-                                well = wellList.get(j);
-                                wellsSampleList = well.copyWellSamples();
-                                well.getId().getValue();
-                                for i = 0:wellsSampleList.size()-1,
-                                    ws = wellsSampleList.get(i);
-                                    ws.getId().getValue();
-                                    % pa = ws.getPlateAcquisition();
-                                    iid = java.lang.Long(ws.getImage().getId().getValue());
-                                    if iid == objId
-                                       obj_type = 'Image';
-                                       return;
-                                    end;
-                                end;
-                            end;
                          end;
                     end;
 end
