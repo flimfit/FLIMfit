3f2160533430791aca5aff9d46ad587ecefadbeb
yalexand@ph-yalexandlap.ic.ac.uk <yalexand@ph-yalexandlap.ic.ac.uk>
2013-06-05 11:39:37 +0100

    ..add forgotten splash function
    --HG--
    branch : dev

---
+++ GlobalProcessingFrontEnd/InternalHelperFunctions/set_splash.m
@@ -0,0 +1,24 @@
+function set_splash(splash_image_filename)
+
+% create a figure that is not visible yet, and has minimal titlebar properties
+fh = figure('Visible','off','MenuBar','none','NumberTitle','off');
+
+% put an axes in it
+ah = axes('Parent',fh,'Visible','off');
+ih = imshow(splash_image_filename);
+
+% set the figure size to be just big enough for the image, and centered at
+% the center of the screen
+imxpos = get(ih,'XData');
+imypos = get(ih,'YData');
+set(ah,'Unit','Normalized','Position',[0,0,1,1]);
+figpos = get(fh,'Position');
+figpos(3:4) = [imxpos(2) imypos(2)];
+set(fh,'Position',figpos);
+movegui(fh,'center')
+
+% make the figure visible
+set(fh,'Visible','on');
+
+end
+
