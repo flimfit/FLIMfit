57e6be6534087c8c17080a48323f3c24de8202b8
Doug Kelly <dougkelly88@googlemail.com>
2013-09-10 17:52:06 +0100

    Removed extraneous lines

--- FLIMfitFrontEnd/Classes/flim_fit_graph_controller.m
+++ FLIMfitFrontEnd/Classes/flim_fit_graph_controller.m
@@ -310,17 +310,6 @@ classdef flim_fit_graph_controller < abstract_plot_controller
                     set(dcm_obj,'UpdateFcn',{@interactive_plot_update,obj,y_scatter,f_scatter,r_scatter,grouping,x_data,hs});                
                 end
                 
-                %{
-                fig = obj.window;
-                
-                data = struct('graph_axes',ax,'x_data',x_data,'y_data',y_data);
-                
-                datacursormode(fig,'on');
-                dcm_obj = datacursormode(fig);
-                set(dcm_obj,'UpdateFcn',@(p1,p2) data_cursor(data,p1,p2))
-                datacursormode(fig,'on');
-                %}
-                
                 hold(ax,'off');
 
                 lims = f.get_cur_lims(param);
@@ -373,62 +362,3 @@ classdef flim_fit_graph_controller < abstract_plot_controller
     
     
 end
-
-function txt = interactive_plot_update(~,event_obj,obj,y_scatter,f_scatter,r_scatter,grouping,x_data,hs)
-    
-%     try
-        pos = get(event_obj,'Position');
-        T = get(event_obj,'Target');
-        
-        dcm_obj = datacursormode(obj.window);
-        dtip = dcm_obj.CurrentDataCursor;
-        set(dtip,'Marker','o','MarkerFaceColor','none','MarkerEdgeColor','g');
-
-        if iscell(x_data)
-            x = x_data{pos(1)};
-        else
-            x = num2str((x_data(x_data == pos(1))));
-        end
-            
-        
-        if T == hs
-            md = obj.fit_controller.fit_result.metadata;
-            
-            if grouping == 2
-                Region = r_scatter(y_scatter == pos(2));
-                FOV = f_scatter(y_scatter == pos(2));
-                Well = md.Well(FOV{1} == [md.FOV{:}]);
-                txt = {[obj.ind_param  ': '  x],...
-                    [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
-                    ['Well: ' Well{:}],...
-                    ['FOV: ' num2str(FOV{:})],...
-                    ['Region: ' num2str(Region)],...
-%                     ['Target: ' num2str(T)]};
-                };
-            elseif grouping == 1 || grouping == 3
-                FOV = f_scatter(y_scatter == pos(2));
-                Well = md.Well(FOV == [md.FOV{:}]);
-                txt = {[obj.ind_param  ': ' x],...
-                    [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
-                    ['Well: ' Well{:}],...
-                    ['FOV:' num2str(FOV)],...
-%                     ['Target: ' num2str(T)]};
-                };
-            elseif grouping == 4
-                Well = f_scatter{y_scatter == pos(2)};
-                txt = {[obj.ind_param ': ', x],...
-                    [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
-                    ['Well: ' Well],...
-%                     ['Target: ' num2str(T)]};
-                };
-            end
-        else    
-            txt = {['Mean ' obj.ind_param  ': ' x], ...
-                [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))]};
-        end
-%     catch err
-%         txt = {['Error in generating datapoint:  ' err]};
-%     end
-
-end
-
