d491230f9ffb1e68dcd11d5b27a0cb3b12cfb2ee
Doug Kelly <dougkelly88@googlemail.com>
2013-09-10 17:21:29 +0100

    Fixed(/work around) error with x variable sometimes being a cell array. Commentd out try-catch to let errors be handled elsewhere?

--- FLIMfitFrontEnd/Classes/flim_fit_graph_controller.m
+++ FLIMfitFrontEnd/Classes/flim_fit_graph_controller.m
@@ -375,12 +375,8 @@ classdef flim_fit_graph_controller < abstract_plot_controller
 end
 
 function txt = interactive_plot_update(~,event_obj,obj,y_scatter,f_scatter,r_scatter,grouping,x_data,hs)
-    %TODO - expand for arbitrary metadata parameters
-    %TODO - turn on and off using contextmenu - also perhaps change tip
-    %style (window/datatip)? Ability to turn off draggable, too?
-    %TODO - call as function from external .m file?
     
-    try
+%     try
         pos = get(event_obj,'Position');
         T = get(event_obj,'Target');
         
@@ -388,14 +384,21 @@ function txt = interactive_plot_update(~,event_obj,obj,y_scatter,f_scatter,r_sca
         dtip = dcm_obj.CurrentDataCursor;
         set(dtip,'Marker','o','MarkerFaceColor','none','MarkerEdgeColor','g');
 
+        if iscell(x_data)
+            x = x_data{pos(1)};
+        else
+            x = num2str((x_data(x_data == pos(1))));
+        end
+            
+        
         if T == hs
             md = obj.fit_controller.fit_result.metadata;
-
+            
             if grouping == 2
                 Region = r_scatter(y_scatter == pos(2));
                 FOV = f_scatter(y_scatter == pos(2));
                 Well = md.Well(FOV{1} == [md.FOV{:}]);
-                txt = {[obj.ind_param  ': ' x_data{pos(1)}],...
+                txt = {[obj.ind_param  ': '  x],...
                     [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
                     ['Well: ' Well{:}],...
                     ['FOV: ' num2str(FOV{:})],...
@@ -405,7 +408,7 @@ function txt = interactive_plot_update(~,event_obj,obj,y_scatter,f_scatter,r_sca
             elseif grouping == 1 || grouping == 3
                 FOV = f_scatter(y_scatter == pos(2));
                 Well = md.Well(FOV == [md.FOV{:}]);
-                txt = {[obj.ind_param  ': ' x_data{pos(1)}],...
+                txt = {[obj.ind_param  ': ' x],...
                     [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
                     ['Well: ' Well{:}],...
                     ['FOV:' num2str(FOV)],...
@@ -413,19 +416,19 @@ function txt = interactive_plot_update(~,event_obj,obj,y_scatter,f_scatter,r_sca
                 };
             elseif grouping == 4
                 Well = f_scatter{y_scatter == pos(2)};
-                txt = {[obj.ind_param ': ', x_data{pos(1)}],...
+                txt = {[obj.ind_param ': ', x],...
                     [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))],...
                     ['Well: ' Well],...
 %                     ['Target: ' num2str(T)]};
                 };
             end
         else    
-            txt = {['Mean ' obj.ind_param  ': ' num2str(pos(1))], ...
+            txt = {['Mean ' obj.ind_param  ': ' x], ...
                 [obj.fit_controller.fit_result.latex_params{obj.cur_param} ': ' sprintf('%6.0f',pos(2))]};
         end
-    catch err
-        txt = {['Error in generating datapoint:  ' err]};
-    end
+%     catch err
+%         txt = {['Error in generating datapoint:  ' err]};
+%     end
 
 end
 
