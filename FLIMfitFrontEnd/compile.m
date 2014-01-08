4a2e72a4c04a7cbd80e813e3db9f0942f5614391
Ian Munro <i.munro@imperial.ac.uk>
2014-01-08 18:29:00 +0000

    Fixes for remote build in Dundee.

--- FLIMfitFrontEnd/compile.m
+++ FLIMfitFrontEnd/compile.m
@@ -195,7 +195,7 @@ function compile(v)
             % examples are included for:
             % Macports GCC 4.7 [FLIMfit_GCC47MP.platypus]
             % Homebrew GCC 4.7 [FLIMfit_GCC47HB.platypus]
-            cmd = ['/usr/local/bin/platypus -y -P FLIMfit_GCC47HB.platypus -a "' package_name '" -V ' v ' ' deploy_folder '/' package_name];
+            cmd = ['/usr/local/bin/platypus -y -P FLIMfit_GCC47.platypus -a "' package_name '" -V ' v ' ' deploy_folder '/' package_name];
             
             
             system(cmd);
