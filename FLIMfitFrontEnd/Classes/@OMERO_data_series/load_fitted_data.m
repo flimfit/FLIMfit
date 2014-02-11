7b32e440c523803171672aa186f951d60c300b8c
yalexand <yalexand@ic.ac.uk>
2014-02-11 16:47:22 +0000

    next couple of eliminations of "whos_Object"

--- FLIMfitFrontEnd/Classes/@OMERO_data_series/load_fitted_data.m
+++ FLIMfitFrontEnd/Classes/@OMERO_data_series/load_fitted_data.m
@@ -124,7 +124,7 @@ function infostring = load_fitted_data(obj,f,~) %f : flim_fit_controller
                 FOV_names = FOV_names_noble;
                 %                                                                 
 
-            table_data = read_analysis_stats_data_from_annotation(obj,object.getId().getValue());
+            table_data = read_analysis_stats_data_from_annotation(obj,object);
             if isempty(table_data), errordlg('FOV statistics annotation file missing - can not continue'), 
                 return, 
             end;
