4af345b526c85f50feac2476e862a71030419319
yalexand <yalexand@ic.ac.uk>
2014-05-22 12:08:11 +0100

    ic_importer - bug fix (filename parsing function call)

--- FLIMfitFrontEnd/ic_importer/ic_importer_impl.m
+++ FLIMfitFrontEnd/ic_importer/ic_importer_impl.m
@@ -1133,7 +1133,7 @@ classdef ic_importer_impl < handle
                       % try to deduce the delays
                       channels_names = cell(1,num_files);
                       for i = 1 : num_files
-                          fnamestruct = obj.parse_DIFN_format1(file_names{i});
+                          fnamestruct = parse_DIFN_format1(file_names{i});
                           channels_names{i} = fnamestruct.delaystr;
                       end
                       %  
@@ -1619,7 +1619,7 @@ classdef ic_importer_impl < handle
                                             %
                                             Z(i,:,:) = squeeze(U(:,:,1))';                            
                                             %
-                                            fnamestruct = obj.parse_DIFN_format1(file_names{i});
+                                            fnamestruct = parse_DIFN_format1(file_names{i});
                                             channels_names{i} = fnamestruct.delaystr; % delay [ps] in string format
                                             %
                                             waitbar(i/num_files, hw);
@@ -1713,7 +1713,7 @@ classdef ic_importer_impl < handle
                                     %                                                            
                                     Z = [];
                                     %
-                                    fnamestruct = obj.parse_DIFN_format1(file_names{i});
+                                    fnamestruct = parse_DIFN_format1(file_names{i});
                                     if ~isempty(fnamestruct)                                    
                                         channels_names = cell(1,num_files);
                                     else
@@ -1737,7 +1737,7 @@ classdef ic_importer_impl < handle
                                             %
                                             Z(i,:,:) = squeeze(U(:,:,1))';                            
                                             %
-                                            fnamestruct = obj.parse_DIFN_format1(file_names{i});
+                                            fnamestruct = parse_DIFN_format1(file_names{i});
                                             if ~isempty(fnamestruct)
                                                 channels_names{i} = fnamestruct.delaystr; % delay [ps] in string format
                                             end
@@ -2138,38 +2138,6 @@ classdef ic_importer_impl < handle
             end
 
         end
-% %-------------------------------------------------------------------------%
-%         function ret = parse_DIFN_format1(obj, DelayedImageFileName, ~)
-% 
-%         ret = [];
-% 
-%             try
-% 
-%                 str = split(' ',DelayedImageFileName);                            
-% 
-%                 if 1 == numel(str)
-% 
-%                     str1 = split('_',DelayedImageFileName);                            
-%                     str2 = char(str1(2));
-%                     str3 = split('.',str2);
-%                         ret.delaystr = num2str(str2num(char(str3(1))));    
-% 
-%                 elseif 2 == numel(str)
-% 
-%                      str = split(' ',DelayedImageFileName);                            
-%                      str1 = char(str(2));     
-%                      str2 = split('_',str1);                            
-%                      str3 = char(str2(2));
-%                      str4 = split('.',str3);
-%                         ret.delaystr = num2str(str2num(char(str4(1))));
-%                      str5 = split('_',char(str(1)));
-%                         ret.integrationtimestr = num2str(str2num(char(str5(2))));                
-%                 end
-% 
-%             catch err
-%                 disp(err.message);
-%             end
-%         end
 %-------------------------------------------------------------------------%
         function upload_Image_BH(obj, dataset, full_filename, ~)
             
