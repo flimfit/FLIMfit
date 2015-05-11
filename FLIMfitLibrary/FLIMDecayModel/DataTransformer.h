2992fb9d100b918438bd74d089d6caa7f857db38
Sean Warren <sean.warren09@imperial.ac.uk>
2015-05-11 18:46:10 +1000

    More work on data transformer

--- FLIMfitLibrary/FLIMDecayModel/DataTransformer.h
+++ FLIMfitLibrary/FLIMDecayModel/DataTransformer.h
@@ -25,6 +25,8 @@ public:
    {
       acq = acq_;
       
+      n_chan = acq->n_chan;
+      
       const std::vector<double>& t_full = acq->GetTimePoints();
       int n_t_full = t_full.size();
       
@@ -39,14 +41,19 @@ public:
             timepoints.push_back(t_full[i]);
       }
       
+      n_t = timepoints.size();
+      n_meas = n_t * n_chan;
    }
+
+   int n_t;
+   int n_meas;
+   int n_chan;
+   int t_skip;
    
 protected:
    
    std::shared_ptr<AcquisitionParameters> acq;
    
-   int n_t;
-   int t_skip;
    std::vector<double> timepoints;
 };
 
@@ -58,8 +65,22 @@ public:
       
    }
 
-   void setImage(std::shared_ptr<FLIMImage> image_) { image = image_; };
-   void setTransformationSettings(DataTransformationSettings& transform_ ) { transform = transform_; };
+   void setImage(std::shared_ptr<FLIMImage> image_)
+   {
+      image = image_;
+      auto acq = image->getAcquisitionParameters();
+      dp = std::make_shared<TransformedDataParameters>(acq, transform);
+      
+      y_smoothed_buf.resize(acq->n_px);
+      tr_row_buf.resize(acq->n_y);
+   };
+
+   void setTransformationSettings(DataTransformationSettings& transform_ )
+   {
+      transform = transform_;
+      if (image != nullptr)
+         setImage(image);
+   };
    
    template <typename T>
    void getTransformedData(T* data);
@@ -70,7 +91,9 @@ public:
 private:
    std::shared_ptr<FLIMImage> image;
    DataTransformationSettings transform;
+   std::shared_ptr<TransformedDataParameters> dp;
    vector<float> tr_row_buf;
+   vector<float> y_smoothed_buf;
 };
 
 template <typename T>
@@ -81,7 +104,7 @@ void DataTransformer::getDataMask(std::vector<uint8_t> final_mask)
    bool has_seg_mask = seg_mask.size() > 0;
 
    auto acq = image->getAcquisitionParameters();
-   
+
    int n_px = acq->n_px;
    int n_meas_full = acq->n_meas_full;
    
@@ -111,7 +134,7 @@ void DataTransformer::getDataMask(std::vector<uint8_t> final_mask)
       }
       
       float bg_val = transform.background->getAverageBackgroundPerGate(p) * n_meas_full;
-      if ((intensity.at<float>(p)-bg_val) < transform.threshold || final_mask[p] < 0 || final_mask[p] >= MAX_REGION)
+      if ((intensity.at<float>(p)-bg_val) < transform.threshold || final_mask[p] < 0)
          final_mask[p] = 0;
       
    }
@@ -122,15 +145,13 @@ void DataTransformer::getDataMask(std::vector<uint8_t> final_mask)
 template <typename T>
 void DataTransformer::getTransformedData(T* tr_data)
 {
-   int idx, tr_idx;
+   //int idx, tr_idx;
    
    auto acq = image->getAcquisitionParameters();
    int n_x = acq->n_x;
    int n_y = acq->n_y;
    int n_chan = acq->n_chan;
-   
-   vector<float>& intensity = intensity_[thread];
-   vector<float>& r_ss = r_ss_[thread];
+   int n_meas_full = acq->n_meas_full;
 
    T* tr_buf = image->getDataPointer<T>();
    T* cur_data_ptr = tr_buf;
@@ -147,32 +168,30 @@ void DataTransformer::getTransformedData(T* tr_data)
          for(int x=0; x<n_x; x++)
             for(int c=0; c<n_chan; c++)
             {
-               for(int i=0; i<n_t; i++)
-                  tr_ptr[i] = cur_data_ptr[t_skip[c]+i];
-               cur_data_ptr += n_t_full;
-               tr_ptr += n_t;
+               for(int i=0; i<dp->n_t; i++)
+                  tr_ptr[i] = cur_data_ptr[dp->t_skip+i];
+               cur_data_ptr += acq->n_t_full;
+               tr_ptr += dp->n_t;
             }
    }
    else
    {
       int s = transform.smoothing_factor;
       
-      int dxt = n_meas;
+      int dxt = dp->n_meas;
       int dyt = n_x * dxt;
       
       int dx = n_meas_full;
       int dy = n_x * dx;
       
-      float* y_smoothed_buf = intensity.data(); // use intensity as a buffer
-      
       float sa = (float) 2*s+1;
       
       for(int c=0; c<n_chan; c++)
       {
-         for(int i=0; i<n_t; i++)
+         for(int i=0; i<dp->n_t; i++)
          {
-            tr_idx = c*n_t + i;
-            idx = c*n_t_full + t_skip[c] + i;
+            int tr_idx = c*dp->n_t + i;
+            int idx = c*acq->n_t_full + dp->t_skip + i;
             
             //Smooth in y axis
             for(int x=0; x<n_x; x++)
@@ -240,80 +259,20 @@ void DataTransformer::getTransformedData(T* tr_data)
       }
    }
    
-   float tvb_sum = 0;
-   if (background_type == BG_TV_IMAGE)
-      for(int i=0; i<n_meas; i++)
-         tvb_sum += tvb_profile[i];
    
-   
-   // Calculate intensity
-   float* intensity_ptr = intensity.data();
-   cur_data_ptr = (T*) tr_buf;
-   for(int p=0; p<n_px; p++)
-   {
-      *intensity_ptr = 0;
-      for(int i=0; i<n_meas_full; i++)
-         *intensity_ptr += cur_data_ptr[i];
-      cur_data_ptr += n_meas_full;
-      
-      if (background_type == BG_VALUE)
-         *intensity_ptr -= background_value * n_meas_full;
-      else if (background_type == BG_IMAGE)
-         *intensity_ptr -= background_image[p] * n_meas_full;
-      else if (background_type == BG_TV_IMAGE)
-         *intensity_ptr -= (tvb_sum * tvb_I_map[p] + background_value * n_meas_full) ;
-      
-      intensity_ptr++;
-   }
-   
-
+   std::shared_ptr<FLIMBackground> background = transform.background;
    
    // Subtract background
-   if (background_type == BG_VALUE)
+   int idx = 0;
+   for(int p=0; p<acq->n_px; p++)
    {
-      int n_tot = n_x * n_y * n_chan * n_t;
-      for(int i=0; i<n_tot; i++)
+      for(int i=0; i<dp->n_meas; i++)
       {
-         tr_data[i] -= background_value * smoothing_area;
-         tr_data[i] *= photons_per_count;
+         tr_data[idx] -= background->getBackgroundValue(p, i);
+         tr_data[idx] *= photons_per_count;
+         if (tr_data[i] < 0) tr_data[i] = 0;
+         idx++;
       }
-   }
-   else if (background_type == BG_IMAGE)
-   {
-      int idx = 0;
-      for(int p=0; p<n_px; p++)
-         for(int i=0; i<n_meas; i++)
-         {
-            tr_data[idx] -= background_image[p] * smoothing_area;
-            tr_data[idx] *= photons_per_count;
-            idx++;
-         }
-   }
-   else if (background_type == BG_TV_IMAGE)
-   {
-      int idx = 0;
-      for(int p=0; p<n_px; p++)
-         for(int i=0; i<n_meas; i++)
-         {
-            tr_data[idx] -= (tvb_profile[i] * tvb_I_map[p] + background_value) * smoothing_area;
-            tr_data[idx] *= photons_per_count;
-            idx++;
-         }
-   }
-   else
-   {
-      int n_tot = n_x * n_y * n_chan * n_t;
-      for(int i=0; i<n_tot; i++)
-         tr_data[i] *= photons_per_count;
-   }
-   
-   
-   // Set negative values to zero
-   int n_tot = n_x * n_y * n_chan * n_t;
-   for(int i=0; i<n_tot; i++)
-   {
-      if (tr_data[i] < 0)
-         tr_data[i] = 0;
    }
    
    image->releasePointer<T>();
