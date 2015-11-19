37a2ff900e829a140099a51c3b13bf1d77c76cfa
Sean Warren <sean.warren09@imperial.ac.uk>
2015-11-19 12:50:06 +1100

    Remove requirement for curl, 7zip and move dependency download into GatherDependencies.ps1

--- Configure.bat
+++ Configure.bat
@@ -10,8 +10,8 @@ choco.exe 2> NUL
 if ERRORLEVEL 9009 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-:: Install cmake, gs and curl, 7zip to download Boost, OMERO stuff
-choco install curl 7zip.commandline cmake.portable -y
+:: Install cmake, gs and OMERO stuff
+choco install cmake.portable -y
 
 :: The following packages must be installed as admin
 IF NOT DEFINED NOADMIN (
@@ -49,51 +49,3 @@ FOR /D %%G in ("%REDIST_STR%") DO (
 	goto skip
 )
 :skip
-
-
-:: Setup Boost
-SET BOOST_URL=http://sourceforge.net/projects/boost/files/boost-binaries/%BOOST_VER_MAJOR%.%BOOST_VER_MINOR%.0/boost_%BOOST_VER_MAJOR%_%BOOST_VER_MINOR%_0-msvc-%MSVC_VER%.0-64.exe/download
-SET BOOST_ROOT=c:\local\boost_%BOOST_VER_MAJOR%_%BOOST_VER_MINOR%_0\
-SET BOOST_LIBRARYDIR=%BOOST_ROOT%lib64-msvc-%MSVC_VER%.0\
-SETX BOOST_ROOT c:\local\boost_%BOOST_VER_MAJOR%_%BOOST_VER_MINOR%_0\ /m
-SETX BOOST_LIBRARYDIR %BOOST_ROOT%lib64-msvc-%MSVC_VER%.0\ /m
-
-
-
-:: Check if boost is installed and install if not
-if exist %BOOST_LIBRARYDIR% (
-	echo Boost already installed
-) else (
-	echo About to download and install Boost from: %BOOST_URL%
-	curl -L %BOOST_URL% -oboost-installer.exe
-	boost-installer.exe /silent
-	del boost-installer.exe
-)
-
-
-SET OMERO_LIBS_FOLDER=FLIMfitFrontEnd\OMEROMatlab\libs\
-
-echo Downloading %OME% version of OMERO.matlab
-curl -OL http://downloads.openmicroscopy.org/latest/omero%OME%/matlab.zip
-7z.exe x matlab.zip -aoa -oFLIMfitFrontEnd/OMEROMatlab/ 
-del matlab.zip
-
-for /f %%i in ('dir FLIMfitFrontEnd\OMEROMatlab\ /ad /b') do set OMERO_FOLDER=%%i
-echo del matlab.zip
-xcopy FLIMfitFrontEnd\OMEROMatlab\%OMERO_FOLDER%\* FLIMfitFrontEnd\OMEROMatlab\ /E /Y
-
-echo remove sl4j-api.jar to avoid LOGGER clashes
-del %OMERO_LIBS_FOLDER%\slf4j-log4j12.jar
-del %OMERO_LIBS_FOLDER%\slf4j-api.jar
-del %OMERO_LIBS_FOLDER%\log4j.jar
-
-
-echo Downloading %OME% version of bfmatlab
-curl -OL http://downloads.openmicroscopy.org/latest/bio-formats%OME%/artifacts/bfmatlab.zip
-7z.exe x bfmatlab.zip -aoa -oFLIMfitFrontEnd/BFMatlab/
-xcopy FLIMfitFrontEnd\BFMatlab\bfmatlab\* FLIMfitFrontEnd\BFMatlab\ /E /Y
-del bfmatlab.zip
-
-:: Download ini4j.jar
-curl -L http://artifacts.openmicroscopy.org/artifactory/maven/org/ini4j/ini4j/0.3.2/ini4j-0.3.2.jar -o%OMERO_LIBS_FOLDER%ini4j.jar
-
---
+++ GatherDependencies.ps1
@@ -0,0 +1,90 @@
+
+$OME = $env:OME
+$MSVC_VER = $env:MSVC_VER
+$BOOST_VER_MAJOR = $env:BOOST_VER_MAJOR
+$BOOST_VER_MINOR = $env:BOOST_VER_MINOR
+
+if (!$OME) { $OME=5.1 }
+if (!$MSVC_VER) { $MSVC_VER=12 }
+if (!$BOOST_VER_MAJOR) { $BOOST_VER_MAJOR=1 }
+if (!$BOOST_VER_MINOR) { $BOOST_VER_MINOR=59 }
+
+
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+	$fc = New-Object -com Scripting.FileSystemObject
+	$shell = New-Object -com Shell.Application
+	$srcfolder = $shell.NameSpace($zipfile)
+	$destfolder = $shell.NameSpace($outpath)
+	$items = $srcfolder.Items()
+	$destfolder.CopyHere($items);
+}
+
+function MoveSubFolderIntoFolder
+{
+	param([string]$path)
+	$shell = New-Object -com Shell.Application
+	$src = gci -path $path | Select-Object -Expand FullName
+	$srcfolder = $shell.NameSpace($src)
+	$destfolder = $shell.NameSpace($path)
+	$items = $srcfolder.Items()
+	$destfolder.MoveHere($items)
+}
+
+function DownloadZipIntoFolder
+{
+	param([string]$url, [string]$output_dir)
+	$output_file = "$pwd\download.zip"
+	echo "Downloading: $url ->" 
+	echo "             $output_dir"
+	Remove-Item -Recurse -Force "$output_dir*"
+	((new-object net.webclient).DownloadFile($url, $output_file))
+	Unzip $output_file $output_dir 
+	MoveSubFolderIntoFolder $output_dir
+	Remove-Item $output_file
+}
+
+$ome_url = 'http://downloads.openmicroscopy.org/latest/omero' + $OME + '/matlab.zip'
+$bf_url = 'http://downloads.openmicroscopy.org/latest/bio-formats' + $OME + '/artifacts/bfmatlab.zip';
+$ini4j_url = 'http://artifacts.openmicroscopy.org/artifactory/maven/org/ini4j/ini4j/0.3.2/ini4j-0.3.2.jar'
+
+$omero_matlab_libs_dir = "$pwd\FLIMfitFrontEnd\OMEROMatlab\libs\"
+
+DownloadZipIntoFolder $ome_url "$pwd\FLIMfitFrontEnd\OMEROMatlab\"
+
+#echo remove sl4j-api.jar to avoid LOGGER clashes
+Remove-Item "$omero_matlab_libs_dir\slf4j-log4j12.jar"
+Remove-Item "$omero_matlab_libs_dir\slf4j-api.jar"
+Remove-Item "$omero_matlab_libs_dir\log4j.jar"
+
+DownloadZipIntoFolder $bf_url "$pwd\FLIMfitFrontEnd\BFMatlab\"
+
+echo "Downloading ini4j.jar"
+((new-object net.webclient).DownloadFile($ini4j_url, "$omero_matlab_libs_dir\ini4j.jar"))
+
+
+echo "Setup Boost"
+$boost_url='http://sourceforge.net/projects/boost/files/boost-binaries/' + $BOOST_VER_MAJOR + '.' + $BOOST_VER_MINOR + '.0/boost_' + $BOOST_VER_MAJOR + '_' + $BOOST_VER_MINOR + '_0-msvc-' + $MSVC_VER + '.0-64.exe/download'
+$BOOST_ROOT="$pwd\Boost\"
+$BOOST_LIBRARYDIR=$BOOST_ROOT + 'lib64-msvc-' + $MSVC_VER + '.0\'
+[Environment]::SetEnvironmentVariable("BOOST_ROOT", $BOOST_ROOT, "User")
+[Environment]::SetEnvironmentVariable("BOOST_LIBRARYDIR", $BOOST_LIBRARYDIR, "User")
+
+echo "Check if boost is installed and install if not"
+echo $BOOST_ROOT
+if (Test-Path $BOOST_LIBRARYDIR)
+{
+	echo "    Boost already installed"
+} 
+else
+{
+	echo "    Downloading: $BOOST_URL"
+	Remove-Item boost-installer.exe
+	$webclient = new-object net.webclient
+	$webclient.DownloadFile($boost_url, "$pwd\boost-installer.exe")
+	.\boost-installer.exe /silent /DIR="$pwd\Boost\"
+}
+
+
+
