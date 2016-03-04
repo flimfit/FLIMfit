fb0d2a79b7f6b04155a18ddf93837dae5713afe1
Ian Munro <i.munro@imperial.ac.uk>
2016-03-04 12:42:10 +0000

    Update default OMERO version for 4.10.4 NB TBD Windows builds.

--- BuildFLIMfit.sh
+++ BuildFLIMfit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z ${OME+x} ]; then export OME=5.1; echo "Setting OME=5.1"; fi
+if [ -z ${OME+x} ]; then export OME=5.2; echo "Setting OME=5.1"; fi
 if [ -z ${MATLAB_VER+x} ]; then export MATLAB_VER=R2015b; echo "Setting MATLAB_VER=R2015b"; fi
 
 export CC=/usr/local/bin/gcc-5
