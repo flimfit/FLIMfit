0707da53e691a1547e03153d2ebc5f61e36459eb
Sean Warren <s.warren@garvan.org.au>
2016-03-22 08:50:34 +1100

    Use more useful dialog for errors

--- FLIMfitFrontEnd/InternalHelperFunctions/escaped_callback.m
+++ FLIMfitFrontEnd/InternalHelperFunctions/escaped_callback.m
@@ -8,8 +8,7 @@ function escaped_callback( ~, ~, fcn)
         catch e
             d = getReport(e,'extended','hyperlinks','off');
             diagnostics('program','error','error_report',d);
-            d = ['An error occurred - please file a bug using the bug tracker in the Help menu providing information about when this error occurred and a screenshot of this message.' char(10) char(10) d];
-            errordlg(d,'Error Occurred');
+            error_dialog(d);
         end
     end
 
