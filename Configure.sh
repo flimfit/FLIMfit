0bde1cff09b563607d80ccf10ae15266f8c5819e
Ian Munro <i.munro@imperial.ac.uk>
2016-03-22 15:53:16 +0000

    Add debug to try and resolve why the beta version of bioformats isn't installing/

--- Configure.sh
+++ Configure.sh
@@ -46,6 +46,7 @@ mv bfmatlab/* FLIMfitFrontEnd/BFMatlab/
 rm -rf bfmatlab
 
 # temporarily download a beta version of bioformats for testing TO BE REMOVED!
+echo "Downloading beta version of bio-formats. TO BE REMOVED!"
 curl -OL https://ci.openmicroscopy.org/job/BIOFORMATS-5.1-merge-build/1128/artifact/artifacts/bioformats_package.jar
 rm FLIMfitFrontEnd/BFMatlab/bioformats_package.jar
 mv bioformat_package.jar FLIMfitFrontEnd/BFMatlab/bioformats_package.jar
