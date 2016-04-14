6ea3895fe6620288734a88415da0e1596d722e79
Ian Munro <i.munro@imperial.ac.uk>
2016-04-14 21:59:02 +0100

    Further changes for #208

--- FLIMfitFrontEnd/Classes/front_end_menu_controller.m
+++ FLIMfitFrontEnd/Classes/front_end_menu_controller.m
@@ -650,7 +650,7 @@ classdef front_end_menu_controller < handle
             
             obj.data_series_controller.data_series = flim_data_series();
             
-            obj.data_series_controller.load_single([path file]);
+            obj.data_series_controller.load_single(path, files);
             if strcmp(obj.default_path,'C:\')
                 obj.default_path = path;
             end
