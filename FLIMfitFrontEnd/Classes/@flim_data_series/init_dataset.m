98d13dfe52f3295fce76a5d5da23ace10609af1f
Ian Munro <i.munro@imperial.ac.uk>
2016-06-22 17:43:30 +0100

    Addresses #248 by using a local background_type so that compute_tr_data is only called once.

--- FLIMfitFrontEnd/Classes/@flim_data_series/init_dataset.m
+++ FLIMfitFrontEnd/Classes/@flim_data_series/init_dataset.m
@@ -33,7 +33,7 @@ function init_dataset(obj,setting_file_name)
     % Set defaults for background depending on type of data
     if strcmp(obj.mode,'TCSPC')
         obj.background_value = 0;
-        obj.background_type = 0;
+        background_type = 0;
     else
         background_val = prof.Data.Default_Camera_Background;
         
@@ -41,10 +41,10 @@ function init_dataset(obj,setting_file_name)
         % data
         if min(obj.cur_tr_data(:)) <= background_val
             obj.background_value = 0;
-            obj.background_type = 0;
+            background_type = 0;
         else
             obj.background_value = background_val;
-            obj.background_type = 1;
+            background_type = 1;
         end
     end
     
@@ -124,6 +124,8 @@ function init_dataset(obj,setting_file_name)
     end
     
     obj.compute_tr_irf();
-    obj.compute_tr_data();
+    % NB this calls compute_tr_data so no need to call explicitly
+    obj.background_type = background_type; 
+    
 
 end
