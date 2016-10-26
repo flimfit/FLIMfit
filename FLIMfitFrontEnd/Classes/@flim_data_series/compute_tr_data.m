c95d1ee39a2564e55659f5628f98a1368949aa14
Ian Munro <i.munro@imperial.ac.uk>
2016-10-26 14:46:42 +0100

    Typo

--- FLIMfitFrontEnd/Classes/@flim_data_series/compute_tr_data.m
+++ FLIMfitFrontEnd/Classes/@flim_data_series/compute_tr_data.m
@@ -47,7 +47,7 @@ function calculated = compute_tr_data(obj,notify_update,no_smoothing)
     
     if obj.init && ~obj.suspend_transformation
         
-        % arbitarary size threshold
+        % arbitrary size threshold
         d_size = obj.data_size;
         if (d_size(1) * d_size(3) * d_size(4)) > 16000000
             wbar = waitbar(1,'Transforming! Please wait...');
