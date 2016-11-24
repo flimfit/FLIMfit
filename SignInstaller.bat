95dd9b5465e04ea0f82ed23949ad4c70078721d6
Sean Warren <s.warren@garvan.org.au>
2016-11-24 11:55:07 +1100

    Add script to sign installer on windows

---
+++ SignInstaller.bat
@@ -0,0 +1,2 @@
+@echo off
+for %%f in (.\FLIMfitStandalone\Installer\*) do "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /debug /v /f "%CERTFILE%" /p "%CERTPASS%" /n "Sean Warren" /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 "%%f"
