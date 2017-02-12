a12a17fc21c20283d41096dff3d8740aaf329384
Sean Warren <s.warren@garvan.org.au>
2017-02-12 20:48:45 +1100

    Display and smooth phasor

--- FLIMfitFrontEnd/phasor_segmentation_controller.m
+++ FLIMfitFrontEnd/phasor_segmentation_controller.m
@@ -101,9 +101,18 @@ classdef phasor_segmentation_controller < flim_data_series_observer
             set(obj.segmentation_axes,'XTick',[],'YTick',[]);
             daspect(obj.segmentation_axes,[1 1 1]);
             
-            obj.phasor_im = image(0,'Parent',obj.phasor_axes);
-            set(obj.phasor_axes,'XTick',[],'YTick',[]);
-            daspect(obj.phasor_axes,[1 1 1]);
+            obj.phasor_im = image(0,'Parent',obj.phasor_axes);            
+            edges = linspace(0,1,256);
+            ed = edges(2:255);
+            obj.phasor_im = imagesc(ed,ed,ones(256,256),'Parent',obj.phasor_axes);
+            daspect(obj.phasor_axes,[1 1 1])
+            set(obj.phasor_axes,'YDir','normal','XTick',[],'YTick',[]);
+
+            hold(obj.phasor_axes,'on');
+            theta = linspace(0,pi,1000);
+            c = 0.5*(cos(theta) + 1i * sin(theta)) + 0.5;
+            plot(obj.phasor_axes,real(c), imag(c) ,'w');
+
 
             obj.update_display();
             obj.slh = addlistener(obj.data_series_list,'selection_updated',@(~,~) escaped_callback(@obj.selection_updated));
@@ -111,11 +120,91 @@ classdef phasor_segmentation_controller < flim_data_series_observer
         end
 
         function update_display(obj)
-        end
-        
-        function on_callback(obj,evt)
+            
+            if isempty(obj.segmentation_axes) || ~ishandle(obj.segmentation_axes) || ~obj.data_series.init
+                return
+            end
+            
+            
+            m = 256;
+
+            selected = obj.data_series_list.selected;
+
+            %trim_outliers = get(obj.trim_outliers_checkbox,'Value');
+
+            cim = obj.data_series_controller.selected_intensity(selected,false);
+            d = obj.data_series_controller.data_series;
+
+            
+            cim = uint8(255 * cim / prctile(cim(:),99));
+            cim = repmat(cim,[1 1 3]);
+            
+            set(obj.segmentation_im,'CData',cim);
+            set(obj.segmentation_axes,'XLim',[1 size(cim,2)],'YLim',[1 size(cim,1)]);
+            
+            p_irf = CalculatePhasor(d.tr_t_irf,d.tr_irf);
+            
+            
+            [p,I] = CalculatePhasor(d.t,d.cur_data,p_irf);
+            
+            
+            kern = ones(3,3);
+            kern = kern / sum(kern(:));
+            pr = conv2(squeeze(real(p)),kern,'same');
+            pi = conv2(squeeze(imag(p)),kern,'same');
+            p = pr + 1i * pi;
+            p = reshape(p,[1 size(p)]);
+            
+            DrawPhasor(p,I,obj.phasor_im);
+
             
         end
+        
+        function on_callback(obj,src,evt)
+            toggles = [obj.tool_roi_rect_toggle 
+               obj.tool_roi_poly_toggle
+               obj.tool_roi_circle_toggle
+               obj.tool_roi_paint_toggle];
+            toggle_type = {'rect','poly','ellipse',obj.brush_width};
+            toggle_fcn = {@flex_roi,@flex_roi,@flex_roi,@paint_roi};
+
+            sz = size(obj.mask);
+            sz = sz(1:2);
+
+            if strcmp(src.State,'on')
+                set(toggles(toggles ~= src),'State','off');
+
+                toggle_fcn = toggle_fcn{toggles == src};
+                obj.flex_h = toggle_fcn(obj.figure1,obj.phasor_axes,toggle_type{toggles == src},sz,@obj.roi_callback);
+                obj.toggle_active = src;
+            else
+                if obj.toggle_active == src && ~isempty(obj.flex_h)
+                    delete(obj.flex_h)
+                end
+            end
+    
+            
+            
+        end
+        
+        function roi_callback(obj,roi_mask)
+            modifier = get(gcbf,'currentmodifier');
+            erase_toggle = get(obj.tool_roi_erase_toggle,'State');
+            erase = strcmp(erase_toggle,'on') || ~isempty(modifier);
+
+            obj.n_regions = obj.n_regions + 1;
+
+            d = obj.data_series;
+            
+            % ...
+
+            obj.update_display();
+
+            delete(obj.flex_h);
+            obj.flex_h = [];
+        end
+            
+            
         
         function ok_pressed(obj,src,evt)
         end
@@ -125,6 +214,11 @@ classdef phasor_segmentation_controller < flim_data_series_observer
         end
         
         function data_update(obj)
+            
+        end
+        
+        function selection_updated(obj) 
+            obj.update_display(); 
         end
         
     end
