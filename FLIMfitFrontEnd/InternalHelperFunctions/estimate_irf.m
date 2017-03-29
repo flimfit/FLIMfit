4f9ab94297d39e8c433ed98248ce30b95aea4c70
Sean Warren <s.warren@garvan.org.au>
2017-03-29 17:33:34 +1100

    Improve IRF estimation interface and use exponential modified gaussian model

--- FLIMfitFrontEnd/InternalHelperFunctions/estimate_irf.m
+++ FLIMfitFrontEnd/InternalHelperFunctions/estimate_irf.m
@@ -1,194 +1,111 @@
-function irf = estimate_irf(t,decay)
-
-    % Copyright (C) 2013 Imperial College London.
-    % All rights reserved.
-    %
-    % This program is free software; you can redistribute it and/or modify
-    % it under the terms of the GNU General Public License as published by
-    % the Free Software Foundation; either version 2 of the License, or
-    % (at your option) any later version.
-    %
-    % This program is distributed in the hope that it will be useful,
-    % but WITHOUT ANY WARRANTY; without even the implied warranty of
-    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    % GNU General Public License for more details.
-    %
-    % You should have received a copy of the GNU General Public License along
-    % with this program; if not, write to the Free Software Foundation, Inc.,
-    % 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-    %
-    % This software tool was developed with support from the UK 
-    % Engineering and Physical Sciences Council 
-    % through  a studentship from the Institute of Chemical Biology 
-    % and The Wellcome Trust through a grant entitled 
-    % "The Open Microscopy Environment: Image Informatics for Biological Sciences" (Ref: 095931).
-
-    % Author : Sean Warren
-
-
-    global fh_ei;
+function [irf_final,t_final,chi2_final] = estimate_irf(td, d, T, fit_ax, res_ax)
+        
+    dt = td(2)-td(1);
+    t = -max(td):1:max(td);
+    dc = [];
     
-    if isempty(fh_ei)
-        fh_ei = figure();
-    else
-        figure(fh_ei);
+    opts = optimset('Display', 'iter', 'MaxFunEvals', 10000);
+
+    tau0 = 4000;
+    sigma0 = 100;
+    lambda0 = 1/100;
+
+    [~,idx] = max(d);
+    t0 = td(idx) - sigma0;
+
+    count = 0;
+    [x,chi2_final] = fminsearch(@fit, [tau0, t0, sigma0, lambda0 0], opts);
+    
+    count = 0;
+    fit(x);
+    
+    % Display Results
+    disp(['t0: ' num2str(x(2)) ' ps']);
+    disp(['sigma: ' num2str(x(3)) ' ps']);
+    disp(['tau: ' num2str(x(1)) ' ps']);
+    
+    irf_final = irfc;
+    t_final = irft;    
+        
+    function chi2 = fit(x)
+        tau = x(1);
+        t0 = x(2);
+        sigma = x(3);
+        lambda = x(4);
+        offset = x(5);
+        I = 1;
+        
+        [dc,irfc,irft] = generate_decay(I, tau, t0, sigma, lambda, offset);
+        
+        I = dc \ d;
+        dc = I * dc;
+        
+        n_free = length(d) - length(x);
+        chi2 = sum((d-dc).^2./d)/n_free;
+        
+        if mod(count,20) == 0
+            label = ['\chi^2 = ' num2str(chi2,4)];
+            if chi2 < 1.3
+                label_color = 'g';
+            else
+                label_color = 'r';
+            end
+            
+            plot(fit_ax,td,d,'ob','MarkerSize',3);
+            hold(fit_ax,'on');
+            plot(fit_ax,td,dc,'-b');
+            plot(fit_ax,irft,irfc/max(irfc)*max(d),'r');
+            xlim(fit_ax,[min(td),max(td)])
+            ylim(fit_ax,[0 1.2*max(d)])
+            hold(fit_ax,'off');
+            set(fit_ax,'Box','off','TickDir','out')
+            xlabel(fit_ax,'Time (ps)');
+            ylabel(fit_ax,'Intensity');
+            text(fit_ax,max(td),1.2*max(d),label,'HorizontalAlignment','right','FontSize',12,'BackgroundColor',label_color);
+            
+            plot(res_ax,td,(d-dc)./sqrt(d)/n_free,'b');
+            set(res_ax,'Box','off','TickDir','out')
+            xlim(res_ax,[min(td),max(td)])
+            drawnow
+        end
+        count = count + 1;
     end
-    
-    decay = decay - min(decay);
-    
-    sel = decay>0;
-decay = decay(sel);
-t = t(sel);
-    
 
-cum_decay = cumsum(decay);
-cum_decay = cum_decay / cum_decay(end);
-
-edge02 = find(cum_decay >  0.02,1,'first');
-edge98 = find(cum_decay >= 0.98,1,'first');
-
-centre = mean(decay .* t) / mean(decay);
-width  = t(edge98) - t(edge02);
-round  = 150;
-slope  = 0.05;
-
-tau1   = 400;
-tau2   = 2000;
-a1     = 0.9;
-
-width = 150;
-
-edge95 = find(cum_decay >= 0.90,1,'first');
-tail_decay = decay(edge95:end);
-tail_decay = tail_decay / tail_decay(1);
-tail_t = t(edge95:end);
-tail_t = tail_t - tail_t(1);
-
-subplot(1,1,1);
-plot(decay);
-
-fminsearch(@exp_objt,[tau1 tau2 a1]);
-
-function r = exp_objt(m)
-   
-    tau1 = m(1);
-    tau2 = m(2);
-    a1 = m(3);
-    
-    dc = a1 * exp(-tail_t/tau1)+ (1-a1) * exp(-tail_t/tau2);
-    
-    r = norm(tail_decay-dc);
-    
-end
-
-
-
-
-nt = length(t);
-
-
-%x0 = [centre width round tau1 tau2 a1 slope];
-x0 = [centre width tau1 tau2 a1];
-%x0 = [1540 2100 150 80 2700 0.99];
-
-opt = optimset('MaxFunEvals',10000);
-x = fminsearch(@(x)objt(x,false), x0,opt);
-
-disp(['Centre = ' num2str(x(1)) ]);
-disp(['Width = ' num2str(x(2)) ]);
-%disp(x(3));
-disp(['Tau = ' num2str(x(3:4))]);
-disp(['A = ' num2str(x(5))]);
-
-[r irf] = objt(x,true);
-
-folder = getpref('GlobalAnalysisFrontEnd','DefaultFolder');
-
-dlmwrite([folder '\est_irf.txt'],[t irf],'\t');
-
-
-function [rn irf] = objt(x,disp)
-   
-    centre = x(1);
-    width = x(2);
-    
-    %round = x(3);
-    tau1 = x(3);   %x(4);
-    tau2 = x(4); %x(5);
-    a1 = x(5); %x(6);
-    
-%    slope = x(7);
-    
-    round = 0;
-    slope = 0;
-
-    irf = make_irf(centre,width,round,slope);
-       
-    d1 = a1 * cv(tau1,irf) + (1-a1) * cv(tau2,irf);
-    
-    w = sqrt(decay);
-    w(w==0) = 1;
-    w = 1./w;
-    
-    decayn = decay/sum(decay);
-    d1 = d1/sum(d1);
-    
-    r = (decayn - d1);
-    r = r(1:end);
-    w = w(1:end);
-    
-    rn = sum(r.*r.*w);
-    
-    if disp
-    subplot(1,3,1)
-    plot(t,[d1 decayn irf]);
-    %ylim([0 0.02]);
-    xlabel('t (ps)');
-    title('Linear Scale')
-    subplot(1,3,2)
-    semilogy(t,[d1 decayn irf]);
-    %ylim([1e-4 0.06]);
-    title('Log Scale')
-    xlabel('t (ps)');
-    legend({'Estimated Decay' 'Measured Decay' 'Computed IRF'})
-    subplot(1,3,3)
-    plot(r);
-    drawnow
+    function [dc,binned_irf,binned_t] = generate_decay(I, tau, mu, sigma, lambda, offset)
+        irf = exp(lambda/2.*(2*mu+lambda*sigma^2-2*t)).*erfc((mu+lambda*sigma^2-t)/(sqrt(2)*sigma));
+        
+        [binned_irf, binned_t] = bin_decay(irf);
+        dc = I * conv_irf(binned_t,binned_irf,tau);
+        dc = dc((end-length(td)+1):end) + offset;
     end
-    
-end
 
-function irf = make_irf(centre,width,round,slope)
+    function [binned_decay, binned_t] = bin_decay(decay)
+        tg = ceil(t / dt);
+        tgm = min(tg);
+        binned_decay = accumarray(tg'-tgm+1,decay');
+        binned_t = tgm*dt + (0:(length(binned_decay)-1))' * dt;
+    end
 
-    %{
-irf = zeros(size(t));
-irf(t>centre-width/2 & t<centre+width/2) = 1;
-
-irf = irf .* ( 1 - ((t-centre).^2./centre^2)*slope );
-
-
-irf_spacing = t(2) - t(1);
-
-tk = -(round*5):irf_spacing:(round*5);
-kern = normpdf(tk,0,round);
-irf = conv(irf,kern,'same');
-%}
-   
-irf = normpdf(t,centre,width);
-    
-irf = irf/sum(irf);
-
-end
-
-function decayc = cv(tau,irf)
+    function D = conv_irf(tg,g,tau)
        
-    decayx = exp(-t/tau);
-    
-    decayc = conv(irf,decayx);
-    decayc = decayc(1:nt);
-    
-%    decayc = decayc/sum(decayc);
-end
-
+        % See thesis page: 69-70
+        
+        rhoi = exp(tg/tau);
+        G = g.*rhoi;
+        G = cumsum(G);
+        G = circshift(G,1);
+        G(1) = 0;
+        rho = exp(dt / tau);
+        
+        A = tau.^2/dt * (1-rho)^2/rho;
+        B = tau.^2/dt * (dt / tau - 1 + 1/rho);
+        
+        C = A * G + B * g .* rhoi;
+        
+        f = 1 / (exp(T/tau)-1);
+        
+        D = (C + f * C(end))./ rhoi / tau * dt;
+        
+    end
 
 end
