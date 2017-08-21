d233b939ff5711f3768ab47bc65aad5cd0208cfe
Sean Warren <s.warren@garvan.org.au>
2017-08-21 16:36:54 +1000

    Make mexutils header only (easier to share)

--- FLIMfitLibrary/FLIMfitMex/MexUtils.h
+++ FLIMfitLibrary/FLIMfitMex/MexUtils.h
@@ -6,37 +6,224 @@
 #include <memory>
 #include <cv.h>
 
+/*
+   Input validation
+*/
+
 #define AssertInputCondition(x) checkInputCondition(#x, x);
-void checkInputCondition(const char* text, bool condition);
+inline void checkInputCondition(const char* text, bool condition)
+{
+   if (!condition)
+      mexErrMsgIdAndTxt("FLIMfitMex:invalidInput", text);
+}
 
-std::string getStringFromMatlab(const mxArray* dat);
+inline void checkSize(const mxArray* array, int needed)
+{
+   if (needed != mxGetNumberOfElements(array))
+      mexErrMsgIdAndTxt("MATLAB:mxmalloc:invalidInput",
+         "Input array is the wrong size");
+}
 
-void checkSize(const mxArray* array, int needed);
-void checkInput(int nrhs, int needed);
+inline void checkInput(int nrhs, int needed)
+{
+   if (nrhs < needed)
+      mexErrMsgIdAndTxt("MATLAB:mxmalloc:invalidInput",
+         "Not enough input arguments");
+}
 
-mxArray* mxCreateUint64Scalar(uint64_t v);
+/*
+   uint64 convenience
+*/
+
+inline mxArray* mxCreateUint64Scalar(uint64_t v)
+{
+   mxArray* m = mxCreateNumericMatrix(1, 1, mxUINT64_CLASS, mxREAL);
+   uint64_t* mp = (uint64_t*)mxGetData(m);
+   mp[0] = v;
+   return m;
+}
+
+/*
+   String handling
+*/
+
+inline std::string getStringFromMatlab(const mxArray* dat)
+{
+   if (mxIsChar(dat))
+   {
+      size_t buflen = mxGetN(dat) * sizeof(mxChar) + 1;
+      char* buf = (char*)mxMalloc(buflen);
+
+      mxGetString(dat, buf, (mwSize)buflen);
+
+      return std::string(buf);
+   }
+
+   mexWarnMsgIdAndTxt("MATLAB:mxmalloc:invalidInput",
+      "Unable to retrieve string");
+
+   return std::string();
+}
 
 /*
    Structure handling
 */
 
-mxArray* getFieldFromStruct(const mxArray* s, const char *field);
-double getValueFromStruct(const mxArray* s, const char *field, double default_value);
-double getValueFromStruct(const mxArray* s, const char *field);
+
+inline mxArray* getFieldFromStruct(const mxArray* s, const char *field)
+{
+   int field_number = mxGetFieldNumber(s, field);
+   if (field_number == -1)
+   {
+      std::string err = std::string("Missing field in structure: ").append(field);
+      mexErrMsgIdAndTxt("FLIMfit:missingField", err.c_str());
+   }
+
+   return mxGetFieldByNumber(s, 0, field_number);
+}
+
+inline double getValueFromStruct(const mxArray* s, const char *field, double default_value)
+{
+   int field_number = mxGetFieldNumber(s, field);
+   if (field_number == -1)
+      return default_value;
+
+   const mxArray* v = mxGetFieldByNumber(s, 0, field_number);
+
+   if (!mxIsScalar(v))
+   {
+      std::string err = std::string("Expected field to be scalar: ").append(field);
+      mexErrMsgIdAndTxt("FLIMfit:missingField", err.c_str());
+   }
+
+   return mxGetScalar(v);
+}
+
+inline double getValueFromStruct(const mxArray* s, const char *field)
+{
+   const mxArray* v = getFieldFromStruct(s, field);
+
+   if (!mxIsScalar(v))
+   {
+      std::string err = std::string("Expected field to be scalar: ").append(field);
+      mexErrMsgIdAndTxt("FLIMfit:missingField", err.c_str());
+   }
+
+   return mxGetScalar(v);
+}
 
 /*
    Argument handling
 */
 
-bool isArgument(int nrhs, const mxArray *prhs[], const char* arg, int nstart = 0);
-const mxArray* getNamedArgument(int nrhs, const mxArray *prhs[], const char* arg, int nstart = 0);
+inline bool isArgument(int nrhs, const mxArray *prhs[], const char* arg, int nstart = 0)
+{
+   for (int i = nstart; (i + 1) < nrhs; i++)
+   {
+      if (mxIsChar(prhs[i]) && getStringFromMatlab(prhs[i]) == arg)
+         return true;
+   }
+   return false;
+}
+
+inline const mxArray* getNamedArgument(int nrhs, const mxArray *prhs[], const char* arg, int nstart = 0)
+{
+   for (int i = nstart; (i + 1) < nrhs; i += 2)
+   {
+      if (mxIsChar(prhs[i]) && getStringFromMatlab(prhs[i]) == arg)
+         return prhs[i + 1];
+   }
+
+   std::string err = std::string("Missing argument: ").append(arg);
+   mexErrMsgIdAndTxt("FLIMfit:missingArgument", err.c_str());
+   return static_cast<const mxArray*>(nullptr);
+}
 
 /*
    OpenCV handling
 */
 
-cv::Mat getCvMat(const mxArray* im);
-mxArray* convertCvMat(const cv::Mat im);
+inline cv::Mat getCvMat(const mxArray* im)
+{
+   int type;
+   if (mxIsDouble(im))
+      type = CV_64F;
+   else if (mxIsSingle(im))
+      type = CV_32F;
+   else if (mxIsInt32(im))
+      type = CV_32S;
+   else if (mxIsInt16(im))
+      type = CV_16S;
+   else if (mxIsInt8(im))
+      type = CV_8S;
+   else if (mxIsUint16(im))
+      type = CV_16U;
+   else if (mxIsUint8(im))
+      type = CV_8U;
+   else
+      mexErrMsgIdAndTxt("FLIMfit:invalidInput",
+         "Image was not of an acceptable type");
+
+   AssertInputCondition(mxIsDouble(im));
+   AssertInputCondition(mxGetNumberOfDimensions(im) == 2);
+   int n = (int) mxGetN(im);
+   int m = (int) mxGetM(im);
+
+   return cv::Mat(n, m, type, mxGetData(im));
+}
+
+inline mxArray* convertCvMat(const cv::Mat im)
+{
+   int type = im.type();
+   int data_size;
+   mxClassID mtype;
+   if (type == CV_64F)
+   {
+      mtype = mxDOUBLE_CLASS;
+      data_size = 8;
+   }
+   else if (type == CV_32F)
+   {
+      mtype = mxSINGLE_CLASS;
+      data_size = 4;
+   }
+   else if (type == CV_32S)
+   {
+      mtype = mxINT32_CLASS;
+      data_size = 4;
+   }
+   else if (type == CV_16S)
+   {
+      mtype = mxINT16_CLASS;
+      data_size = 2;
+   }
+   else if (type == CV_16U)
+   {
+      mtype = mxUINT16_CLASS;
+      data_size = 2;
+   }
+   else if (type == CV_8S)
+   {
+      mtype = mxINT8_CLASS;
+      data_size = 1;
+   }
+   else if (type == CV_8U)
+   {
+      mtype = mxUINT8_CLASS;
+      data_size = 1;
+   }
+   else
+      mexErrMsgIdAndTxt("FLIMfit:invalidInput",
+         "Image was not of an acceptable type");
+
+   mxArray* a = mxCreateNumericMatrix(im.rows, im.cols, mtype, mxREAL);
+   char* ptr = reinterpret_cast<char*>(mxGetData(a));
+
+   for (int i = 0; i < im.rows; i++)
+      memcpy(ptr + i*im.cols*data_size, im.data + i*im.step, im.cols*data_size);
+
+   return a;
+}
 
 /*
   Vector conversion
