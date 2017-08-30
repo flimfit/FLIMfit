92f48d9573b87f78ffbea142858a2bdb20d18a0d
Sean Warren <s.warren@garvan.org.au>
2017-08-30 10:42:58 +1000

    Use VS2017 by default

--- Configure.bat
+++ Configure.bat
@@ -1,6 +1,6 @@
 @echo off
 
-IF NOT DEFINED MSVC_VER SET MSVC_VER=14
+IF NOT DEFINED MSVC_VER SET MSVC_VER=15
 
 :: Install Chocolatey
 choco.exe 2> NUL
@@ -8,10 +8,10 @@ if ERRORLEVEL 9009 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex 
 SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 :: Install cmake, gs and OMERO stuff
-choco install cmake.portable -y -version 3.7.0
 
 :: The following packages must be installed as admin
 IF NOT DEFINED NOADMIN (
+   choco install cmake.install
 	choco install innosetup -y
 	choco install ghostscript.app -y -version 9.16
 
@@ -19,21 +19,6 @@ IF NOT DEFINED NOADMIN (
 	curl -LO http://www.sherlocksoftware.org/innotools/files/itd0.3.5.exe
 	itd0.3.5.exe /silent
 	del itd0.3.5.exe
-
-	:: Check if requested version of Visual Studio is installed
-	echo H: !VS%MSVC_VER%0COMNTOOLS!
-	IF NOT DEFINED VS%MSVC_VER%0COMNTOOLS (
-		IF MSVC_VER==14	( choco install visualstudio2015community -y  -packageParameters "--Features MDDCPlusPlus"
-		) ELSE (
-			IF MSVC_VER==12 ( choco install visualstudio2013community -y
-			) ELSE (
-				IF MSVC_VER==11 (choco install visualstudio2012wdx -y
-				) ELSE ( 
-					choco install visualstudio2015community -y  -packageParameters "--Features MDDCPlusPlus"
-					SET MSVC_VER=14
-				)
-			)
-		)
 	)
 	Endlocal & SET MSVC_VER=%MSVC_VER%
 
