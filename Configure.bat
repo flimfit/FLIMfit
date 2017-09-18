cdab873bb87e934628267b5d6d93665c32f7daf1
Sean Warren <s.warren@garvan.org.au>
2017-09-18 13:37:56 +1000

    Fix VC redist path

--- Configure.bat
+++ Configure.bat
@@ -22,8 +22,8 @@ IF NOT DEFINED NOADMIN (
 	del idpsetup-1.5.1.exe
 )
 
-IF %MSVC_VER% EQU 15 SET REDIST_STR=%PROGRAMFILES(x86)%Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.11.25325\
-IF %MSVC_VER% NEQ 15 SET REDIST_STR=%PROGRAMFILES(x86)%Microsoft Visual Studio %MSVC_VER%.0\VC\redist\*
+IF %MSVC_VER% EQU 15 SET REDIST_STR=%PROGRAMFILES(x86)%\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.11.25325
+IF %MSVC_VER% NEQ 15 SET REDIST_STR=%PROGRAMFILES(x86)%\Microsoft Visual Studio %MSVC_VER%.0\VC\redist\*
 
 ECHO %REDIST_STR%\vcredist_x64.exe>FLIMfitLibrary\VisualStudioRedistributablePath.txt
 
