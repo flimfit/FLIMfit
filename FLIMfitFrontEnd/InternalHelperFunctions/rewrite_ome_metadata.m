5c1b070c929e4e7bf28a35db0d0939b7c4683f33
Sean Warren <s.warren@garvan.org.au>
2017-09-22 09:44:16 +1000

    FIx OME metadata rewrite for mac

--- FLIMfitFrontEnd/InternalHelperFunctions/rewrite_ome_metadata.m
+++ FLIMfitFrontEnd/InternalHelperFunctions/rewrite_ome_metadata.m
@@ -2,11 +2,11 @@ function rewrite_ome_metadata(folder)
 % Set SizeT and SizeC to 1 in Lavision metadata
 % Can 'fix' Lavision autosave files with incorrect channel order
 
-    files = dir([folder '\*.ome.tif']);
+    files = dir([folder filesep '*.ome.tif']);
 
     for i=1:length(files)
 
-        t = Tiff([folder '\' files(i).name],'r+');
+        t = Tiff([folder filesep files(i).name],'r+');
         info = getTag(t,'ImageDescription');
 
         info = regexprep(info,'SizeT="\d+" SizeC="\d+"','SizeT="1" SizeC="1"');
