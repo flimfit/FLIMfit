86181acdcde7ef3fbe6d344f2cad40d3c1c80a33
Sean Warren <s.warren@garvan.org.au>
2018-02-28 16:20:35 +1100

    Move FLIMreader for compatibility with 6.0 branch

--- cmake/Hunter/config.cmake
+++
@@ -1,2 +0,0 @@
-hunter_config(OpenCV VERSION ${HUNTER_OpenCV_VERSION} CMAKE_ARGS WITH_CUDA=OFF WITH_OPENEXR=OFF WITH_EIGEN=OFF WITH_JASPER=OFF WITH_AVFOUNDATION=OFF BUILD_opencv_python=OFF WITH_LAPACK=OFF)
-hunter_config(Boost VERSION 1.61.0) # override manually for the moment - v1.64 not building correctly on VS2017
