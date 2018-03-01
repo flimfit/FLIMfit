f6c62a60eb3d4b8c51a41ba74ec373f7085a4eef
Sean Warren <s.warren@garvan.org.au>
2018-03-01 19:21:39 +1100

    Build FlimReader separately to main dll to use gcc for openmp and appleclang for flimreader(cuda)

--- BuildFLIMfit.sh
+++ BuildFLIMfit.sh
@@ -2,30 +2,54 @@
 
 if [ -z ${MATLAB_VER+x} ]; then export MATLAB_VER=R2017b; echo "Setting MATLAB_VER=R2017b"; fi
 
-export CC=/usr/local/bin/gcc-7
-export CXX=/usr/local/bin/g++-7
-
-export CC=/usr/local/opt/llvm/bin/clang
-export CXX=/usr/local/opt/llvm/bin/clang++
+# Build FlimReader Mex file
+#--------------------------------------------
+project_dir=GeneratedProjects/FlimReaderUnix
 
 echo "Cleaning CMake Project..."
-#rm -rf GeneratedProjects/Unix
-#mkdir -p GeneratedProjects/Unix
+rm -rf ${project_dir}
+mkdir -p ${project_dir}
 
 cur_dir=$(grealpath .)
 
 echo "Generating CMake Project..."
-if ! cmake -H. -BGeneratedProjects/Unix -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE \
+if ! cmake -HFLIMfitLibrary/FLIMreader -B${project_dir} -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
    -DFlimReaderMEX_OUT_DIR=${cur_dir}/FLIMfitLibrary/Libraries/; then 
    echo 'Error generating project'
    exit 1
 fi
 
 echo "Building Project..."
-if ! cmake --build GeneratedProjects/Unix --config Release; then
+if ! cmake --build ${project_dir}; then
     echo 'Error building project'
     exit 1
 fi
+
+# Build FLIMfit library
+#--------------------------------------------
+
+export CC=/usr/local/bin/gcc-7
+export CXX=/usr/local/bin/g++-7
+
+echo "Cleaning CMake Project..."
+rm -rf GeneratedProjects/Unix
+mkdir -p GeneratedProjects/Unix
+
+cur_dir=$(grealpath .)
+
+echo "Generating CMake Project..."
+if ! cmake -H. -BGeneratedProjects/Unix -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE; then 
+   echo 'Error generating project'
+   exit 1
+fi
+
+echo "Building Project..."
+if ! cmake --build GeneratedProjects/Unix; then
+    echo 'Error building project'
+    exit 1
+fi
+
+exit 0;
 
 export PATH=/Applications/MATLAB_${MATLAB_VER}.app/bin:$PATH
 # compile the Matlab code to generate the FLIMfit_MACI64.app
