5ffc3dfdc1444fedb34b8b5bcd45a3e6cf777f2a
Sean Warren <s.warren@garvan.org.au>
2018-03-02 09:36:38 +1100

    Don't pull lapack into levmar - we're not using it in the ML algorithm anyway

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -12,9 +12,8 @@ if %MSVC_VER%==15 SET MSVC_YEAR=2017
 
 echo Cleaning CMake Project
 SET PROJECT_DIR=GeneratedProjects\MSVC%MSVC_VER%_64
-REM rmdir %PROJECT_DIR% /s /q
+rmdir %PROJECT_DIR% /s /q
 mkdir %PROJECT_DIR%
-cd %PROJECT_DIR%
 
 echo Setting up vcpkg paths
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
@@ -26,17 +25,14 @@ if %MSVC_VER%==15 (set GENERATOR="Visual Studio %MSVC_VER% Win64"
 
 echo Generating CMake Project in: %PROJECT_DIR%
 echo Using Generator: %GENERATOR%
-cmake -G %GENERATOR% ..\..\ -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"
+cmake -G %GENERATOR% -H. -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
 echo Building 64bit Project in Release mode
-cmake --build . --config Release
+cmake --build %PROJECT_DIR% --config Release
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
-cd "..\..\"
 echo Compiling front end
 echo Please wait for MATLAB to load
 
-cd FLIMfitFrontEnd
-"C:\Program Files\MATLAB\%MATLAB_VER%\bin\matlab.exe" -nosplash -nodesktop -wait -log compile_output.txt -r "cd('%CD%'); compile(true); quit();"
-cd ..
+"C:\Program Files\MATLAB\%MATLAB_VER%\bin\matlab.exe" -nosplash -nodesktop -wait -log compile_output.txt -r "cd('%CD%\FLIMfitFrontEnd'); compile(true); quit();"
