8e19d2937c17045ab03ede3854dcc9919d422cc8
Sean Warren <s.warren@garvan.org.au>
2018-03-02 09:41:37 +1100

    Build flimreader seperately on windows as well

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -10,11 +10,6 @@ if %MSVC_VER%==12 SET MSVC_YEAR=2013
 if %MSVC_VER%==14 SET MSVC_YEAR=2015
 if %MSVC_VER%==15 SET MSVC_YEAR=2017
 
-echo Cleaning CMake Project
-SET PROJECT_DIR=GeneratedProjects\MSVC%MSVC_VER%_64
-rmdir %PROJECT_DIR% /s /q
-mkdir %PROJECT_DIR%
-
 echo Setting up vcpkg paths
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
@@ -23,14 +18,30 @@ SET TOOLCHAIN_FILE=%TOOLCHAIN_FILE:\=/%
 if %MSVC_VER%==15 (set GENERATOR="Visual Studio %MSVC_VER% Win64"
 ) else set GENERATOR="Visual Studio %MSVC_VER% %MSVC_YEAR% Win64"
 
-echo Generating CMake Project in: %PROJECT_DIR%
-echo Using Generator: %GENERATOR%
+:: Build main library
+echo Cleaning CMake Project
+SET PROJECT_DIR=GeneratedProjects\MSVC%MSVC_VER%_64
+rmdir %PROJECT_DIR% /s /q
+mkdir %PROJECT_DIR%
+echo Generating CMake Project in: %PROJECT_DIR%, using %GENERATOR%
 cmake -G %GENERATOR% -H. -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
-
 echo Building 64bit Project in Release mode
 cmake --build %PROJECT_DIR% --config Release
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
+
+:: Build FlimReader mex file
+echo Cleaning CMake Project
+SET PROJECT_DIR=GeneratedProjects\MSVC%MSVC_VER%_64_FLIMreader
+rmdir %PROJECT_DIR% /s /q
+mkdir %PROJECT_DIR%
+echo Generating CMake Project in: %PROJECT_DIR%, using %GENERATOR%
+cmake -G %GENERATOR% -HFLIMfitLibrary/FLIMreader -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"
+if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
+echo Building 64bit Project in Release mode
+cmake --build %PROJECT_DIR% --config Release
+if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
+
 
 echo Compiling front end
 echo Please wait for MATLAB to load
