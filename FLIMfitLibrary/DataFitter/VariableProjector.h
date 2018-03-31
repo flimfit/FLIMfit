8bdba5bc44c0171ee3c1311e14596559251e1d89
Sean Warren <s.warren@garvan.org.au>
2018-03-31 19:55:31 +1100

    Start to apply active set method to variable projection derivatives

--- FLIMfitLibrary/DataFitter/VariableProjector.h
+++ FLIMfitLibrary/DataFitter/VariableProjector.h
@@ -15,19 +15,24 @@ public:
    VariableProjector(VariableProjector&) = delete;
    VariableProjector(VariableProjector&&) = default;
 
+   void setActiveColumns(const std::vector<bool>& active);
+
 protected:   
    void setData(float* y);
    void setNumResampled(int nr_) { nr = nr_; }
-   void transformAB();
+   void transformAB(const std::vector<int>& inc);
    void backSolve();
    void weightModel();
+   void weightActiveModel();
    void computeJacobian(const std::vector<int>& inc, double *residual, double *jacobian);
 
    double d_sign(double *a, double *b);
 
-   int n, nmax, ndim, nl, l, p, pmax, philp1, nr;
+   int n, nmax, ndim, nl, l, la,  p, pmax, philp1, nr;
    
    std::vector<double> work, aw, bw, u, r;
+
+   std::vector<bool> active;
 
    std::shared_ptr<std::vector<double>> a, b, wp;
 
