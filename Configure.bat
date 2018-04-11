e5ad648d760f85fc9161eb95d42e3a4d0857f49f
Sean Warren <s.warren@garvan.org.au>
2018-04-12 09:20:13 +1000

    Generate correct redistributable path for visual studio version in use

--- Configure.bat
+++ Configure.bat
@@ -1,7 +1,5 @@
 @echo off
 
-IF NOT DEFINED MSVC_VER SET MSVC_VER=15
-
 IF DEFINED INSTALL_REQUIRED (
 	:: Please install chocolatey first from : https://chocolatey.org/
    :: The following packages must be installed as admin
@@ -17,9 +15,3 @@ IF DEFINED INSTALL_REQUIRED (
 )
 
 :: vcpkg install opencv[core,tiff]:x64-windows-static boost:x64-windows-static dlib:x64-windows-static
-
-IF %MSVC_VER% EQU 15 SET REDIST_STR=%PROGRAMFILES(x86)%\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.10.25008
-IF %MSVC_VER% NEQ 15 SET REDIST_STR=%PROGRAMFILES(x86)%\Microsoft Visual Studio %MSVC_VER%.0\VC\redist\*
-
-ECHO %REDIST_STR%\vcredist_x64.exe>FLIMfitLibrary\VisualStudioRedistributablePath.txt
-
