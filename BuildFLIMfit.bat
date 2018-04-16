22e14e772b678b23d2be702fa08541169dfa5da7
Sean Warren <s.warren@garvan.org.au>
2018-04-16 18:25:06 +1000

    Place libraries in FLIMfitFrontEnd for easier packaging

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -2,7 +2,7 @@
 
 SETLOCAL
 
-IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2016b
+IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2016b 
 IF NOT DEFINED MSVC_VER SET MSVC_VER=15
 
 if %MSVC_VER%==14 SET MSVC_YEAR=2015
@@ -34,7 +34,7 @@ rmdir %PROJECT_DIR% /s /q
 mkdir %PROJECT_DIR%
 echo Generating CMake Project in: %PROJECT_DIR%, using %GENERATOR%
 cmake -G %GENERATOR% -HFLIMfitLibrary\FLIMreader -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" -DNO_CUDA=1^
-      -DVCPKG_TARGET_TRIPLET=x64-windows-static -DFlimReaderMEX_OUT_DIR="%CD%\FLIMfitLibrary\Libraries" -DCMAKE_BUILD_TYPE=Release
+      -DVCPKG_TARGET_TRIPLET=x64-windows-static -DFlimReaderMEX_OUT_DIR="%CD%\FLIMfitFrontEnd\Libraries" -DCMAKE_BUILD_TYPE=Release
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 echo Building 64bit Project in Release mode
 cmake --build %PROJECT_DIR% --config Release
