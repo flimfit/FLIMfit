25d58291810848b5aa4bc5d9ba56eb4b7bf3510d
Sean Warren <s.warren@garvan.org.au>
2018-06-04 19:05:47 +1000

    Update FLIMreader

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -2,7 +2,7 @@
 
 SETLOCAL
 
-IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2016b
+IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2018a
 
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
@@ -12,15 +12,15 @@ ECHO %TOOLCHAIN_FILE%
 
 echo Cleaning CMake Project
 SET PROJECT_DIR=GeneratedProjects\MSVC15_64
-REM rmdir %PROJECT_DIR% /s /q
-REM mkdir %PROJECT_DIR%
+::rmdir %PROJECT_DIR% /s /q
+mkdir %PROJECT_DIR%
 
 echo Generating CMake Project in: %PROJECT_DIR%
-cmake -G "Visual Studio 15 Win64" -H. -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"
+cmake -G "Visual Studio 15 Win64" -H. -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" -DNO_CUDA=1
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
 echo Building 64bit Project in Release mode
-cmake --build %PROJECT_DIR%  --config Release
+cmake --build %PROJECT_DIR%  --config RelWithDebInfo
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
 echo Compiling front end
