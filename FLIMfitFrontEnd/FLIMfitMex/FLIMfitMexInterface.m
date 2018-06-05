a33ad7ad921b2d65dbf8988a2090a21a2e31fe18
Sean Warren <s.warren@garvan.org.au>
2018-06-05 11:22:56 +1000

    Compatibility with Matlab R2016b

--- FLIMfitFrontEnd/FLIMfitMex/FLIMfitMexInterface.m
+++ FLIMfitFrontEnd/FLIMfitMex/FLIMfitMexInterface.m
@@ -1,6 +1,6 @@
 function [varargout] = FLIMfitMexInterface(module,varargin)
     [msg,varargout{1:nargout}] = FLIMfitMex(module,varargin{:});
-    if ~strcmp(msg,"OK")
-        throw(MException("FLIMFit:mexError",msg));
+    if ~strcmp(msg,'OK')
+        throw(MException('FLIMFit:mexError',msg));
     end
 end
--- FLIMfitFrontEnd/FlimReader.m
+++ FLIMfitFrontEnd/FlimReader.m
@@ -1,6 +1,6 @@
 function [varargout] = FlimReader(varargin)
     [msg,varargout{1:nargout}] = FlimReaderMex(varargin{:});
-    if ~strcmp(msg,"OK")
-        throw(MException("FlimReader:mexError",msg));
+    if ~strcmp(msg,'OK')
+        throw(MException('FlimReader:mexError',msg));
     end
 end
