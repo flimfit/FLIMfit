0ae892f27916cbed7abf18c6fb8219bec3931ed2
Sean Warren <s.warren@garvan.org.au>
2018-07-12 20:17:12 +1000

    Update denoising

--- FLIMfitFrontEnd/denoise.m
+++ FLIMfitFrontEnd/denoise.m
@@ -12,21 +12,43 @@ function settings = denoise(filename,settings)
     data = reader.read([1 1 1],1:reader.n_channels);
     t = reader.delays;
 
-    denoised = zeros(size(data),'single');
-    denoised = single(data);
-
+%    denoised = zeros(size(data),'single');
+%    denoised = single(data);
     
-    for i=1:size(data,1)
-        for j=1:size(data,2)
-            disp(num2str([i, j]));
-            slice = squeeze(double(data(i,j,:,:)));
-            if sum(slice) > 0
-                slice = iterVSTpoisson(slice);
+    mode = 'FastHyDe';
+    
+    if strcmp(mode,'FastHyDe')
+
+        sz = size(data);
+        data = permute(data, [3 4 1 2]);
+        data = reshape(data, [sz(3:4) sz(1)*sz(2)]);
+
+        s = sum(sum(data,2),1) > 0;
+        s = s(:);
+
+        sel_data = data(:,:,s);
+
+        sel_denoised = FastHyDe(double(sel_data), 'additive', 0, 5);
+
+        denoised = zeros(size(data));
+        denoised(:,:,s) = sel_denoised;
+        denoised = single(denoised);
+
+        denoised = reshape(denoised,[sz(3:4) sz(1:2)]);
+        denoised = permute(denoised,[3 4 1 2]);
+    else
+        
+        for i=1:size(data,1)
+            for j=1:size(data,2)
+                disp(num2str([i, j]));
+                slice = squeeze(double(data(i,j,:,:)));
+                if sum(slice) > 0
+                    slice = iterVSTpoisson(slice);
+                end
+                denoised(i,j,:,:) = single(slice);
             end
-            denoised(i,j,:,:) = single(slice);
         end
     end
-
     
     writeFfh([path filesep file '_denoised.ffh'],denoised,t);
 
