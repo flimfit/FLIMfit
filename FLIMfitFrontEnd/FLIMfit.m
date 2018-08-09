2f55661a33e773ac95201cff912fff7288ee0437
Sean Warren <s.warren@garvan.org.au>
2018-08-09 10:00:56 +1000

    Remove outdated bug workaround

--- FLIMfitFrontEnd/FLIMfit.m
+++ FLIMfitFrontEnd/FLIMfit.m
@@ -29,13 +29,6 @@ function ui = FLIMfit()
 
 addpath_global_analysis();
 
-% workaround for this bug: https://www.mathworks.com/support/bugreports/1293244
-if ispc;
-    javax.swing.UIManager.setLookAndFeel('com.sun.java.swing.plaf.windows.WindowsLookAndFeel');
-else ismac;
-    javax.swing.UIManager.setLookAndFeel('com.apple.laf.AquaLookAndFeel');
-end
-
 ui = flim_fit_ui(false);
 check_version(true);
 
