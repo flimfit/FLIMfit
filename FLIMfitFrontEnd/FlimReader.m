33888076bdef0675fee56027beb457c7622ba364
Sean Warren <s.warren@garvan.org.au>
2018-09-21 08:23:55 +1000

    Update FlimReader

--- FLIMfitFrontEnd/FlimReader.m
+++
@@ -1,6 +0,0 @@
-function [varargout] = FlimReader(varargin)
-    [msg,varargout{1:nargout}] = FlimReaderMex(varargin{:});
-    if ~strcmp(msg,'OK')
-        throw(MException('FlimReader:mexError',msg));
-    end
-end
