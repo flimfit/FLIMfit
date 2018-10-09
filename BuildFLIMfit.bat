978212c31ab2ac3bdeeece91cc0cb6c872ecef6f
Sean Warren <s.warren@garvan.org.au>
2018-10-09 15:49:43 +1100

    Add build clean option on windows

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -6,14 +6,13 @@ IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2018b
 
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
-REM SET TOOLCHAIN_FILE=%TOOLCHAIN_FILE:\=/%
 
-ECHO %TOOLCHAIN_FILE%
-
-echo Cleaning CMake Project
 SET PROJECT_DIR=GeneratedProjects\MSVC15_64
-::rmdir %PROJECT_DIR% /s /q
-mkdir %PROJECT_DIR%
+IF "%1"=="--clean" (
+   echo Cleaning CMake Project
+   rmdir %PROJECT_DIR% /s /q
+   mkdir %PROJECT_DIR%
+)
 
 echo Generating CMake Project in: %PROJECT_DIR%
 cmake -G "Visual Studio 15 Win64" -H. -B%PROJECT_DIR% -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" -DNO_CUDA=1
