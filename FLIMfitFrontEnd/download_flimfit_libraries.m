8d7dcaf66dae4282ff7b74692a58f06c92a48c54
Sean Warren <s.warren@garvan.org.au>
2019-01-30 12:17:28 +1100

    Do not delete old libraries until new libs have been successfully downloaded

--- FLIMfitFrontEnd/download_flimfit_libraries.m
+++ FLIMfitFrontEnd/download_flimfit_libraries.m
@@ -6,6 +6,8 @@ function download_flimfit_libraries(prompt_user)
     
     if isdeployed; return; end
 
+    try
+    
     ver = get_git_version();
 
     if exist(['Libraries' filesep 'FLIMfitMex.' mexext ],'file')
@@ -15,28 +17,35 @@ function download_flimfit_libraries(prompt_user)
         lib_ver = fileread(['Libraries' filesep 'LibraryVersion.txt']);
         if strcmp(lib_ver,ver); return; end
         
-        disp(['Old library version found (' lib_ver '), clearing']);
-        clear FLIMfitMex FlimReader
-        delete('Libraries/*.*');
-
     end
         
     if prompt_user
-       answer = questdlg('FLIMfit libraries are not compiled, would you like to download them?','FLIMfit Libraries','Yes','No','Yes');
+       answer = questdlg('Latest FLIMfit libraries are not compiled, would you like to download them?','FLIMfit Libraries','Yes','No','Yes');
        if strcmp(answer,'No'); return; end
     end
-
+    
     platform = lower(computer());
 
     url = ['https://storage.googleapis.com/flimfit-downloads/latest/' ver '/libraries/flimfit_libraries_' platform '_' ver '.zip'];
     file = 'libraries.zip';
 
     websave(file,url);
+    
+    clear FLIMfitMex FlimReader
+    delete('Libraries/*.*');
+
     unzip(file);
 
     f = fopen(['Libraries' filesep 'LibraryVersion.txt'],'w');
     fwrite(f,ver);
     fclose(f);
+    
+    catch e
+        
+        disp('Could not update libraries');
+        disp(e.message)
+        
+    end
 
     
 end
