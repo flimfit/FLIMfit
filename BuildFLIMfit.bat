4887f0cec8832abd0df9ca069d41450ce29bc2c0
Sean Warren <s.warren@garvan.org.au>
2019-05-13 10:50:05 +1000

    Update build script

--- BuildFLIMfit.bat
+++ BuildFLIMfit.bat
@@ -2,13 +2,26 @@
 
 SETLOCAL
 
+SET VS_VERSION=15
+SET VS_YEAR=2017
+
+SET VSCOMMUNITYCMD="C:\Program Files (x86)\Microsoft Visual Studio\%VS_YEAR%\Community\Common7\Tools\VsDevCmd.bat"
+SET VSBUILDCMD="C:\Program Files (x86)\Microsoft Visual Studio\%VS_YEAR%\BuildTools\Common7\Tools\VsDevCmd.bat"
+
+:: Set up Visual Studio environment variables
+IF EXIST %VSCOMMUNITYCMD% CALL %VSCOMMUNITYCMD% -arch=amd64 && GOTO :BUILD
+IF EXIST %VSBUILDCMD% CALL %VSBUILDCMD% -arch=amd64 && GOTO :BUILD
+ECHO Error: Visual Studio install not found && EXIT /B 1
+
+:BUILD
+
 IF NOT DEFINED MATLAB_VER SET MATLAB_VER=R2018b
 
 SET TRIPLET=x64-windows-static
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\%TRIPLET%\bin;%VCPKG_ROOT%\installed\%TRIPLET%\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 
-SET PROJECT_DIR=GeneratedProjects\MSVC15_64
+SET PROJECT_DIR=GeneratedProjects\VS%VS_YEAR%
 IF "%1"=="--clean" (
    echo Cleaning CMake Project
    rmdir %PROJECT_DIR% /s /q
@@ -16,7 +29,7 @@ IF "%1"=="--clean" (
 )
 
 echo Generating CMake Project in: %PROJECT_DIR%
-cmake -G "Visual Studio 15 Win64" -H. -B%PROJECT_DIR%^
+cmake -G"Visual Studio %VS_VERSION% %VS_YEAR%" -A x64 -H. -B%PROJECT_DIR%^
    -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%"^
    -DNO_CUDA=1^
    -DVCPKG_TARGET_TRIPLET=%TRIPLET%^
