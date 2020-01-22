83eaa27cd02c95f93510aadd220163f34f4f46cb
Sean Warren <sean.c.warren@gmail.com>
2020-01-22 13:28:56 +0000

    Fix typo

--- BuildFLIMfit.sh
+++ BuildFLIMfit.sh
@@ -18,7 +18,7 @@ TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
 [ "$1" == "--clean" ] && rm -rf GeneratedProjects/Unix
 if ! cmake -GNinja -H. -BGeneratedProjects/Unix \
     -DOpenMP_omp_LIBRARY=$MATLAB_OMP_ROOT/libiomp5.dylib \
-    -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" then
+    -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE"; then
    echo 'Error configuring project'
    exit 1
 fi
